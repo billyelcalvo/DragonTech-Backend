5532c05337ad79a76fa39776e97f341f89472ebe
billyelcalvo <dam0806blas@gmail.com>
2026-01-07 01:32:25 -0500

    chore : add indexces for performance on tables

--- src/dragontech/infraestructure/database/schemas/schemas.ts
+++ src/dragontech/infraestructure/database/schemas/schemas.ts
@@ -1,9 +1,15 @@
-import { double, mysqlTable, varchar } from "drizzle-orm/mysql-core";
+import { double, index, mysqlTable, varchar } from "drizzle-orm/mysql-core";
 
 export const user = mysqlTable("user", {
     id_user: varchar('id_user', { length: 36 }).primaryKey(),
     email: varchar('email').unique().notNull(),
     password: varchar('password', { length: 60 }).notNull()
+}, (table) => ({
+    idx_email: index("idx_email").on(table.email)
+}));
+export const category = mysqlTable("category", {
+    id_category: varchar("id_category", { length: 36 }).primaryKey(),
+    category_name: varchar("category_name").notNull().unique()
 });
 
 export const product = mysqlTable("product", {
@@ -11,19 +17,22 @@ export const product = mysqlTable("product", {
     name_product: varchar("name_product").notNull().unique(),
     price: double("price").notNull(),
     rating: double("rating"),
-    discount_price: double("discount_price")
-});
+    discount_price: double("discount_price"),
+    id_category: varchar("id_category", { length: 36 }).references(() => category.id_category)
+}, (table) => ({
+    idx_price: index("idx_price").on(table.price),
+    idx_rating: index("idx_rating").on(table.rating),
+    idx_id_category: index("idx_id_category").on(table.id_category)
+}));
 
 export const review = mysqlTable("review", {
     id_review: varchar('id_review', { length: 36 }).primaryKey(),
     review_rating: double('review_rating'),
     comment: varchar('comment', { length: 255 })
-});
+}, (table) => ({
+    idx_review_rating: index("idx_review_rating").on(table.review_rating)
+}));
 
-export const category = mysqlTable("category", {
-    id_category: varchar("id_category", { length: 36 }).primaryKey(),
-    category_name: varchar("category_name").notNull().unique()
-});
 
 export const brand = mysqlTable("brand", {
     id_brand: varchar("id_brand", { length: 36 }).primaryKey(),
